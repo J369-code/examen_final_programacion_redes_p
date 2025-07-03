5ad752e1059eeabb29a8daff648956e9f318e059
J369-code <cruzfloresabeljosue@gmail.com>
2025-07-03 14:49:29 +0000

    Cambios realizados desde la rama features-final-examn

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '!hola desde mi aplicacion docker INF-651'
+    return 'hOLA DESDE MI RAMA DE EXAMEN fINAL INF-650'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
